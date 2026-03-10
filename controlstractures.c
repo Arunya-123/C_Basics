1907c0d37dc7787645c8b7adc9f0ab004f8ae591
ARUNYAMOL GK <arunyamolgk@gmail.com>
2026-03-10 15:42:10 +0530

    Your commit message

---
+++ controlstractures.c
@@ -0,0 +1,19 @@
+# include <stdio.h>
+int main(){
+    int num =23.7;
+    float numm= 45.7;
+    int age=34;
+    if (num>numm){
+        printf("you are correct");
+    }else if (num==numm) {
+        printf("please check the inputs");
+    }else{
+        printf("hai");
+    }
+    (age>=18)? printf("u r allowed"):printf(" u are not allowed");
+    
+
+
+    
+    return 0;
+}
